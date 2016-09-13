7388a34adba444d6913a4bfd112e2271d38e6fdb
shellbot <studio.shellbot@gmail.com>
2016-09-13 03:58:13 -0400

    Started command line utility

---
+++ peachy/__main__.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+def startproject(project_name):
+    project_dirs = [
+        '/assets', '/assets/images', '/assets/sounds',
+        '/build',
+        '/game', '/game/entities', '/game/stages', '/game/worlds'
+    ]
+
+    print("Creating %s project..." % project_name)
+    if not os.path.exists(project_name):
+        os.makedirs(project_name)
+        for subdir in project_dirs:
+            os.makedirs(project_name + subdir)
+    else:
+        print("[ERROR] %s directory already exists" % project_name)
+    
+
+def main():
+    command = sys.argv[1]
+    if command == 'startproject':
+        project_name = sys.argv[2]
+        startproject(project_name)
+
+if __name__ == '__main__':
+    main()
